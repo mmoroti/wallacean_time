e3452019c6a053e69572e62942e68bfe4588360c
mmoroti <mmoroti@gmail.com>
2024-08-07 14:42:44 -0300

    script with filter conditions and nested_df

--- 01_data_cleaned/01_data_cleaned.R
+++ 01_data_cleaned/01_data_cleaned.R
@@ -8,7 +8,7 @@ install.load.package <- function(x) {
 
 ## names of packages we want installed (if not installed yet) and loaded
 package_vec <- c(
-  "rgbif",
+  "cowplot",
   "tidyverse",
   "here"
 )
@@ -33,7 +33,7 @@ View(data_tetrapodstraits %>%
        filter(taxonRank == "UNRANKED")) 
 
 # key of each occurence
-anyDuplicated(data_tetrapodstraits$gbifID)
+anyDuplicated(data_tetrapodstraits$gbifID) 
 
 data_tetrapods_rgbif <- data_tetrapodstraits %>%
   select("speciesKey", "class", "order", "family", "species",
@@ -47,7 +47,6 @@ data_tetrapods_rgbif <- data_tetrapodstraits %>%
   select(-"occurrenceStatus", -"taxonRank") %>%
   mutate(across(everything(), replace_empty_with_na))
 
-
 nrow(data_tetrapods_rgbif) # 1.965.555 occurences species
 table(data_tetrapods_rgbif$class)
 glimpse(data_tetrapods_rgbif)
@@ -59,17 +58,88 @@ data_tetrapods_rgbif %>%
             missing_year = sum(is.na(year)), 
             missing_date = sum(is.na(eventDate)))
 
+# exists a difference between "year" and "event date" of 6041 occurences
+# these differences is associate a occurences between imprecise dates
+# check documentation https://encurtador.com.br/XmP8P
+View(data_tetrapods_rgbif %>%
+       filter(!is.na(eventDate) & is.na(year)))
+
+# select occurences at
+year_filter <- 2020
+
+data_tetrapods_filter <- data_tetrapods_rgbif %>% 
+  drop_na(decimalLatitude,
+          decimalLongitude,
+          year) %>%
+  filter(year <= year_filter)
+
+data_tetrapods_filter %>%
+  summarise(missing_lat = sum(is.na(decimalLatitude)),
+            missing_long = sum(is.na(decimalLongitude)), 
+            missing_year = sum(is.na(year)), 
+            missing_date = sum(is.na(eventDate))) # without missing data =)
+
+nrow(data_tetrapods_filter)
+
+# comparative plots of original vs filtered dataframes 
+df_filter <- as.data.frame(table(data_tetrapods_filter$year)) %>%
+  rename(Year = Var1)
+df_original <- as.data.frame(table(data_tetrapods_rgbif$year)) %>%
+  rename(Year = Var1) 
+
+# Convert the Year column to numeric if it's a factor
+df_filter$Year <- as.numeric(as.character(df_filter$Year))
+df_original$Year <- as.numeric(as.character(df_original$Year)) 
+
+df_comparative <- full_join(df_original, df_filter,
+          by = "Year") %>%
+  rename(Original = Freq.x,  Filtered = Freq.y) %>%
+  filter(Year <= year_filter) %>%
+  pivot_longer(cols = c(Original, Filtered), 
+               names_to = "Dataset",
+               values_to = "Occurences") %>%
+  replace_na(list(Occurences = 0))
+
+ggplot(df_comparative, aes(x = Year, y = Occurences, fill = Dataset)) +
+  geom_bar(stat = "identity", position = "dodge", width = 2, alpha = 0.6) +
+  geom_line(stat = "smooth", aes(color = Dataset), size = 1.0, linetype = "dashed") + 
+  theme_bw() +
+  labs(title = "",
+       y = "Frequency of occurences")
+
 # nested dataframe
-data_tetrapods_nested <- data_tetrapods_rgbif %>%
+data_tetrapods_nested <- data_tetrapods_filter %>%
   group_by(species) %>%
   nest(event_table = c("gbifID", "eventDate", "year", "countryCode", "locality",
                        "stateProvince","decimalLatitude", "decimalLongitude",
                        "coordinateUncertaintyInMeters","coordinatePrecision")) %>%
   ungroup() %>%
-  arrange("speciesKey", "class", "order", "family", "species")
+  arrange(class, order, family, species)
 
+# check data
+table(data_tetrapods_nested$class)
+table(data_tetrapods_nested$order)
 anyDuplicated(data_tetrapods_nested$species) # nenhum nome repetido
-View(data_tetrapods_nested %>% filter(species == "Boana polytaenia"))
+anyDuplicated(data_tetrapods_nested$speciesKey)
 
-View(data_tetrapods_nested %>%
-     filter(duplicated(species)))
+# counting events and ordering years
+data_tetrapods_nested$count_events <- NA
+
+for (i in 1:nrow(data_tetrapods_nested)) {
+  data_tetrapods_nested[i,"count_events"] <- nrow(
+    data_tetrapods_nested$event_table[[i]])
+  
+  data_tetrapods_nested$event_table[[i]] <- 
+    data_tetrapods_nested$event_table[[i]][
+      order(data_tetrapods_nested$event_table[[i]]$year), 
+    ]
+}
+
+data_tetrapods_nested %>%
+  filter(count_events > 10)
+
+save(data_tetrapods_nested, 
+     file = here(
+       "01_data_cleaned",
+       "data_tetrapods_nested.RData")
+)
