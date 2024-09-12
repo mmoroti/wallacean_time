0e33c373165e7de9463604371b48ef902dad9c3b
mmoroti <mmoroti@gmail.com>
2024-09-12 16:15:32 -0300

    splink, biotime e gbif ok

--- 00_raw_data/00_raw_data.R
+++ 00_raw_data/00_raw_data.R
@@ -27,22 +27,32 @@ email <- "mmoroti@gmail.com" # your email
 # DATA FROM RGBIF USING TETRAPODTRAITS AS BACKBONE ----
 TetraData<- data.table::fread("00_raw_data/TetrapodTraits_1.0.0.csv")
 
+# if you need to rerun backbone, use this
 # if we download with usageKey instead of speciesKey, we will use exactly
 # the code number corresponding to the species, for example, in TetrapodTraits
 # we have Hypsiboas polytaenius instead of Boana polytaenia, so we will 
 # download all records of Hypsiboas polytaenius, because it has a different 
 # usageKey than Boana polytaenia the key that defines the "current" identity 
 # of the species, in speciesKey
-species_list_tetrapods <- TetraData %>% 
-  pull("Scientific.Name") %>% # use fewer names if you want to just test 
-  name_backbone_checklist() 
+#species_list_tetrapods <- TetraData %>% 
+#  pull("Scientific.Name") %>% # use fewer names if you want to just test 
+#  name_backbone_checklist() 
 
 # only matchType with matchType EXACT or FUZZY 
 species_list_tetrapods_key <- species_list_tetrapods %>%
   dplyr::filter(!is.na(speciesKey)) %>%
   filter(!matchType == "NONE" & !matchType == "HIGHERRANK") %>%
   pull(speciesKey) 
-  
+
+# confering species
+species_list_tetrapods_filter <- species_list_tetrapods %>%
+       dplyr::filter(!is.na(speciesKey)) %>%
+       filter(!matchType == "NONE" & !matchType == "HIGHERRANK") %>%
+       dplyr::relocate(verbatim_name, .before = canonicalName)
+
+View(species_list_tetrapods_filter %>%
+    filter(confidence < 96))
+
 # query
 occ_download(
   pred_in("taxonKey", species_list_tetrapods_key),
@@ -58,6 +68,7 @@ occ_download(
   user=user,pwd=pwd,email=email
 )
 
+# past
 occ_download_wait('0046879-240626123714530')
 
 # 11-09-2024
@@ -200,23 +211,26 @@ be {(nrow(data_tetrapods_rgbif) - nrow(data_tetrapodstraits)) - nrow(blank_rgbif
 
 # DATASET BioTIME -----
 rm(list=ls()); gc() # clean local enviroment
-# aqui vou identificar as especies no BioTime pela speciesKey do gbif para 
-# dar o join entre os dois datasets, fazendo com que as nomenclaturas fiquem
-# homogeneizadas 
-biotime_data_raw <- data.table::fread(here(
+
+# if you don't needs to rerun backbone, use this
+load(file.path(
+  "00_raw_data",
+  "biotime_data.RData"))
+
+biotime_data_raw <- data.table::fread(file.path(
   "00_raw_data", 
   "BioTIMEQuery_24_06_2021",
   "BioTIMEQuery_24_06_2021.csv"),
   stringsAsFactors=T)
 
-#biotime_data <- biotime_data_raw %>%
-#  select(STUDY_ID, GENUS_SPECIES, LATITUDE, LONGITUDE, YEAR)
+# if you need to rerun backbone, use this
+#species_list_biotime <- biotime_data_raw %>% 
+#  distinct(GENUS_SPECIES, .keep_all = TRUE) %>%
+#  mutate(GENUS_SPECIES = as.character(GENUS_SPECIES)) %>%
+#  pull("GENUS_SPECIES") %>% 
+#  name_backbone_checklist()
 
-species_list_biotime <- biotime_data_raw %>% 
-  distinct(GENUS_SPECIES, .keep_all = TRUE) %>%
-  mutate(GENUS_SPECIES = as.character(GENUS_SPECIES)) %>%
-  pull("GENUS_SPECIES") %>% 
-  name_backbone_checklist()
+hist(species_list_biotime$confidence)
 
 tetrapodsKey <- c(212,    # aves
                11418114, # testudines
@@ -229,35 +243,55 @@ tetrapodsKey <- c(212,    # aves
 species_list_biotime_key <- species_list_biotime %>%
   dplyr::filter(!matchType == "NONE" & !matchType == "HIGHERRANK") %>%
   dplyr::filter(classKey %in% tetrapodsKey) %>%
-  dplyr::select(verbatim_name, speciesKey) %>% 
+  dplyr::select(verbatim_name, speciesKey, confidence, matchType) %>% 
   dplyr::filter(!is.na(speciesKey)) # remover os que nao tem chave, 
                                     # pois nao foram encontrados
                                     # correspondentes no gbif
+
+hist(species_list_biotime_key$confidence) 
+
+# aqui temos especies que aparecem com 'aff.' 'cf.' '?' e tambem serao removidas
+# para evitar adicionar dados com imprecisão taxonômica
+species_list_biotime_key_precleaned <- species_list_biotime_key %>%
+  dplyr::relocate(verbatim_name, .after = matchType) %>% 
+  dplyr::filter(!grepl("aff\\.|cf\\.", verbatim_name)) %>% # remove cf. & aff.
+  dplyr::filter(!grepl("gr\\.|sp\\.", verbatim_name)) %>% # remove cf. & aff.
+  dplyr::filter(!grepl("\\?", verbatim_name)) %>% # remove qualquer interrogacao
+  dplyr::filter(!grepl("\\baff\\b", verbatim_name)) %>% # remove aff separado do txt
+  dplyr::filter(!grepl("\\bsp\\b", verbatim_name)) %>% # remove cf separado do txt
+  dplyr::filter(confidence > 95) 
+
+glue::glue("Quantidade de nomes dúbios, com intervalo de confianca de >0.95 \\
+ou sem correspondência com o backbone: { \\
+nrow(species_list_biotime_key)-nrow(species_list_biotime_key_precleaned)}")
 
 biotime_data_key <- right_join(
   biotime_data_raw,
   species_list_biotime_key,
   by = c("GENUS_SPECIES" = "verbatim_name"))
 
+biotime_data_key_precleaned <- right_join(
+  biotime_data_raw,
+  species_list_biotime_key_precleaned,
+  by = c("GENUS_SPECIES" = "verbatim_name")) 
+
 save(species_list_biotime, # backbone do rgbif
-     species_list_biotime_key, # lista das spp no biotime + chaves
-     biotime_data_key, # dataset biotime filtrado pelo backbone
-     file = here(
+     biotime_data_key_precleaned, # lista das spp no biotime + chaves
+     biotime_data_key_precleaned, # dataset biotime filtrado pelo backbone
+     file = file.path(
        "00_raw_data",
        "biotime_data.RData")
 )
-load(file.path(
-  "00_raw_data",
-  "biotime_data.RData"))
 
 # DATASET specieslink -----
 rm(list=ls()); gc() # clean local enviroment
 
-# if don't necessary rerun backbone
+# if you don't needs rerun backbone, run this
 load(file.path(
   "00_raw_data",
-  "splinks_backbonelist.RData"))
+  "splinks_data.RData"))
 
+# raw data splinks
 splink_data_raw <- data.table::fread(file.path(
   "00_raw_data", 
   "speciesLink-20240909151829-0009656",
@@ -280,11 +314,11 @@ table(splink_data$kingdom)
 table(splink_data$taxonclass)
 
 # if you need rerun name_backbone_checklist, use this
-species_list_splink <- splink_data %>% 
-  distinct(scientificname, .keep_all = TRUE) %>%
-  mutate(scientificname = as.character(scientificname)) %>%
-  pull("scientificname") %>% 
-  name_backbone_checklist()
+#species_list_splink <- splink_data %>% 
+#  distinct(scientificname, .keep_all = TRUE) %>%
+#  mutate(scientificname = as.character(scientificname)) %>%
+#  pull("scientificname") %>% 
+#  name_backbone_checklist()
 
 names(species_list_splink)
 
@@ -297,24 +331,57 @@ tetrapodsKey <- c(212,    # aves
                   359       # mammals
 )
 
+# apenas correspondencias fuzzy ou exact in matchType
+# classKey correspondente a cada taxa
+# remover aquelas sem a chave speciesKey
 species_list_splink_key <- species_list_splink %>%
   dplyr::filter(!matchType == "NONE" & !matchType == "HIGHERRANK") %>%
   dplyr::filter(classKey %in% tetrapodsKey) %>%
-  dplyr::select(verbatim_name, speciesKey) %>% 
+  dplyr::select(verbatim_name, speciesKey, confidence, matchType) %>% 
   dplyr::filter(!is.na(speciesKey)) # remover os que nao tem chave, 
                                     # pois nao foram encontrados
                                     # correspondentes no gbif
-names(species_list_splink_key)
+
+hist(species_list_splink_key$confidence) 
+
+# aqui temos especies que aparecem com 'aff.' 'cf.' '?' e tambem serao removidas
+# para evitar adicionar dados com imprecisão taxonômica
+species_list_splink_key_precleaned <- species_list_splink_key %>%
+  dplyr::relocate(verbatim_name, .after = matchType) %>% 
+  dplyr::filter(!grepl("aff\\.|cf\\.", verbatim_name)) %>% # remove cf. & aff.
+  dplyr::filter(!grepl("gr\\.|sp\\.", verbatim_name)) %>% # remove cf. & aff.
+  dplyr::filter(!grepl("\\?", verbatim_name)) %>% # remove qualquer interrogacao
+  dplyr::filter(!grepl("\\baff\\b", verbatim_name)) %>% # remove aff separado do txt
+  dplyr::filter(!grepl("\\bcf\\b", verbatim_name)) %>% # remove cf separado do txt
+  dplyr::filter(verbatim_name != "Bokermannohyla saxicola (sp nov.)") %>%
+  dplyr::filter(confidence > 95) 
+
+glue::glue("Quantidade de nomes dúbios, com intervalo de confianca de >0.95 \\
+ou sem correspondência com o backbone: { \\
+nrow(species_list_splink_key)-nrow(species_list_splink_key_precleaned)}")
+
+#View(species_list_splink_key_precleaned)
+
+names(species_list_splink_key_precleaned)
+
 splink_data_key <- right_join(
   splink_data,
   species_list_splink_key,
   by = c("scientificname" = "verbatim_name"))
 
-View(splink_data_key)
+splink_data_key_precleaned <- right_join(
+  splink_data,
+  species_list_splink_key_precleaned,
+  by = c("scientificname" = "verbatim_name"))
 
-save(species_list_splink, 
-     species_list_splink_key, 
-     splink_data_key, 
+glue::glue("Quantidade de ocorrências que correspondiam a nomes dúbios,\\
+com intervalo de confiança <0.96 na correspondência de nomes\\
+ou sem correspondência com o backbone do GBIF: { \\
+nrow(splink_data_key)-nrow(splink_data_key_precleaned)}")
+
+save(species_list_splink, # backbone splink
+     species_list_splink_key_precleaned, # name splink and respective speciesKey
+     splink_data_key_precleaned, # match with records in splink
      file = file.path(
       "00_raw_data",
        "splinks_data.RData"))
